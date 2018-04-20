7691bcd513fa8931af72b77cb053cb31119ad9b5
zy <18612332440@163.com>
2018-04-20 13:57:39 +0800

    zy

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,36 +1,44 @@
 <template>
-    <div class="home">
-        首页
-    </div>
+	<div>
+		<div class="home">
+			首页
+		</div>
+		<div>测试</div>
+		<button @click="jq()">2222</button>
+	</div>
+
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        data () {
-            return {
-                test: ''
-            }
-        },
-        methods:{
-            queryData(){
-                /*this.$http.post('http://www.baidu.com', {}, {
-                    headers: {
+	export default {
+		data() {
+			return {
+				test: ''
+			}
+		},
+		methods: {
+			queryData() {
+				/*this.$http.post('http://www.baidu.com', {}, {
+				    headers: {
 
-                    },
-                    emulateHTTP:true
-                }).then(function(res) {
-                    // 这里是处理正确的回调
-                    console.log(res.data);
-                }, function(res) {
-                    // 这里是处理错误的回调
-                    console.log(res.status);
-                });*/
-            }
-        },
-        mounted() {
-            this.queryData();
-        },
-    };
+				    },
+				    emulateHTTP:true
+				}).then(function(res) {
+				    // 这里是处理正确的回调
+				    console.log(res.data);
+				}, function(res) {
+				    // 这里是处理错误的回调
+				    console.log(res.status);
+				});*/
+			},
+			jq(){
+				console.log($('.home'))
+			}
+		},
+		mounted() {
+			this.queryData();
+		},
+	};
 </script>
 
 <style lang="css">
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,30 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import common from 'pages/common';
-import login from 'pages/login/login';
-import home from 'pages/home/home';
-import land from 'pages/land/land';
-
+//懒加载  首页加载速度
 Vue.use(Router);
 
-const routes = [
-  {path: '/', redirect: '/index/home'},
-  {path: '/index', component: common,redirect:'/index/home',
-    children: [
-      {path:'home', component :home,},
-      {path:'land',component:land},
-        ]
-      },
-  {path: '/login', component: login}
-];
-
-export default new Router({
-    /*linkActiveClass: 'active',*/
-    routes
-});
+const routes = new Router({
+	routes: [{
+			path: '/',
+			redirect: '/index/home'
+		},
+		{
+			path: '/index',
+			component: resolve => require(["../pages/common"], resolve),
+			children: [{
+					path: 'home',
+					component: resolve => require(["../pages/home/home"], resolve),
+				},
+				{
+					path: 'land',
+					component: resolve => require(["../pages/land/land"], resolve)
+				},
+			]
+		},
+		{
+			path: '/login',
+			component: resolve => require(["../pages/login/login"], resolve)
+		}
+	]
+})
+export default routes
